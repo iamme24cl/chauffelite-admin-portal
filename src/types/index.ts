8f1240cf234ce702b0330f39e2c11abace1e0619
Chandra Lama <iamme24cl@gmail.com>
2025-05-05 03:12:48 -0700

    UPdated driver and user types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,27 @@
-export interface Driver {
+interface User {
   id: string;
   name: string;
   phone: string;
+  email: string;
 }
+
+export interface Driver {
+  id: string;
+  company_id: string;
+  user: User; 
+  created_at: string;
+  updated_at: string;
+}
+
+export type DriverFormInput = {
+  user:
+    {
+      name: string;
+      phone: string;
+      email?: string;
+      password?: string;
+    } 
+};
+
+
+
