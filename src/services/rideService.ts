ba30516115c8b667303db8b819ac5d1429d5ee78
Chandra Lama <iamme24cl@gmail.com>
2025-05-13 12:14:27 -0700

    add api calls to update ride and assign driver to ride.

--- src/services/rideService.ts
+++ src/services/rideService.ts
@@ -4,3 +4,13 @@ export const fetchRides = async () => {
   const res = await axiosInstance.get("/rides");
   return res.data;
 } 
+
+export const updateRideStatus = async (rideId: string, status: string) => {
+  const res = await axiosInstance.patch(`/rides/${rideId}/status`, { status });
+  return res.data;
+}
+
+export const assignDriverToRide = async (rideId: string, driverId: string) => {
+  const res = await axiosInstance.patch(`/rides/${rideId}/assign`, { driver_id: driverId });
+  return res.data;
+}
